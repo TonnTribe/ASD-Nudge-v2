edbb1cd401ec42b2fdb9429c66eb5340f7403490
Trevor Hanks <trevor.hanks89@gmail.com>
2023-06-29 20:42:05 -0500

    updait folder

--- server.js
+++ server.js
@@ -34,8 +34,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
